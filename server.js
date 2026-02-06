e2d50266f7fdfe292b0cfd174865f638c1032e05
Jayanth421 <164364265+Jayanth421@users.noreply.github.com>
2026-02-06 20:35:01 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,10 +2,20 @@ const express = require("express");
 const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
+const path = require("path");
 
-app.use(express.static("public"));
+/* Serve public files */
+app.use(express.static(path.join(__dirname, "public")));
 
+/* OPEN CAST PAGE BY DEFAULT */
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+/* WebRTC signaling */
 io.on("connection", socket => {
+
+    console.log("User connected");
 
     socket.on("offer", data => {
         socket.broadcast.emit("offer", data);
@@ -19,9 +29,14 @@ io.on("connection", socket => {
         socket.broadcast.emit("candidate", data);
     });
 
+    socket.on("disconnect", () => {
+        console.log("User disconnected");
+    });
+
 });
 
-http.listen(process.env.PORT || 3000, () => {
-    console.log("Server running...");
+/* Start server */
+const PORT = process.env.PORT || 3000;
+http.listen(PORT, () => {
+    console.log("Server running on port " + PORT);
 });
-
